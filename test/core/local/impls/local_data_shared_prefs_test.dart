d5b2ad5e4144b55bf0db747dfd24ec89a2bf8fc4
Chatty <chatti13.chatter@gmail.com>
2024-07-16 02:53:46 +0530

    Added accidentaly

--- test/core/local/impls/local_data_shared_prefs_test.dart
+++ test/core/local/impls/local_data_shared_prefs_test.dart
@@ -1,24 +1,24 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flux_store/src/core/local/impls/local_data_shared_prefs_impl.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flux_store/src/core/local/impls/local_data_shared_prefs_impl.dart';
+// import 'package:mocktail/mocktail.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-class MockSharedPrefsImpl extends Mock implements SharedPreferences {}
+// class MockSharedPrefsImpl extends Mock implements SharedPreferences {}
 
-void main() {
-  late LocalDataSharedPrefsImpl localDataSharedPrefsImpl;
-  late MockSharedPrefsImpl mockSharedPrefsImpl;
+// void main() {
+//   late LocalDataSharedPrefsImpl localDataSharedPrefsImpl;
+//   late MockSharedPrefsImpl mockSharedPrefsImpl;
 
-  setUp(() {
-    mockSharedPrefsImpl = MockSharedPrefsImpl();
-    localDataSharedPrefsImpl = LocalDataSharedPrefsImpl(preferences: mockSharedPrefsImpl);
-  });
+//   setUp(() {
+//     mockSharedPrefsImpl = MockSharedPrefsImpl();
+//     localDataSharedPrefsImpl = LocalDataSharedPrefsImpl(preferences: mockSharedPrefsImpl);
+//   });
 
-  group('set data', () {
-    test('boolean keys should be set to the provided values', () async {      
-      when(() => mockSharedPrefsImpl.setBool("key", false)).thenAnswer((_) async => true);          
-    });
+//   group('set data', () {
+//     test('boolean keys should be set to the provided values', () async {      
+//       when(() => mockSharedPrefsImpl.setBool("key", false)).thenAnswer((_) async => true);          
+//     });
 
 
-  });
-}
+//   });
+// }
